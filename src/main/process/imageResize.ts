f11e48b6fb1c7a6419f4cf437448ee469f09875e
masuhajime <masuhajime@gmail.com>
2023-12-16 02:51:43 +0900

    imageResize base on longer/shorter side

--- src/main/process/imageResize.ts
+++ src/main/process/imageResize.ts
@@ -4,6 +4,12 @@ import { ImageResizeParameter } from './dto';
 const imageResize = async (param: ImageResizeParameter): Promise<Buffer> => {
   const { buffer, method, resizeBase, size } = param;
   const image = sharp(buffer);
+  // get image size
+  const metadata = await image.metadata();
+  const { width, height } = metadata;
+  if (width === undefined || height === undefined) {
+    throw new Error('Image size is undefined');
+  }
 
   // const kernels = keyof sharp.KernelEnum;
   let kernel;
@@ -27,15 +33,55 @@ const imageResize = async (param: ImageResizeParameter): Promise<Buffer> => {
       kernel = sharp.kernel.nearest;
       break;
   }
-  if (resizeBase === 'width') {
-    image.resize(size, null, {
-      kernel,
-    });
-  } else {
-    image.resize(null, size, {
-      kernel,
-    });
+  switch (resizeBase) {
+    case 'width':
+      image.resize(size, null, {
+        kernel,
+      });
+      break;
+    case 'height':
+      image.resize(null, size, {
+        kernel,
+      });
+      break;
+    case 'longer':
+      if (width > height) {
+        image.resize(size, null, {
+          kernel,
+        });
+      } else {
+        image.resize(null, size, {
+          kernel,
+        });
+      }
+      break;
+    case 'shorter':
+      if (width < height) {
+        image.resize(null, size, {
+          kernel,
+        });
+      } else {
+        image.resize(size, null, {
+          kernel,
+        });
+      }
+
+      break;
+    default:
+      image.resize(size, null, {
+        kernel,
+      });
+      break;
   }
+  // if (resizeBase === 'width') {
+  //   image.resize(size, null, {
+  //     kernel,
+  //   });
+  // } elseif (resizeBase === 'width') {
+  //   image.resize(null, size, {
+  //     kernel,
+  //   });
+  // }
 
   return image.toBuffer();
 };
