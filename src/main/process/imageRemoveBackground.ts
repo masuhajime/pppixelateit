8cd069e86917d44b342959a7594884d45e6c1dba
masuhajime <masuhajime@gmail.com>
2024-02-16 22:52:19 +0900

    remove imgly remove background due to fail run on Win

--- src/main/process/imageRemoveBackground.ts
+++ src/main/process/imageRemoveBackground.ts
@@ -1,4 +1,4 @@
-import { removeBackground } from '@masuhajime/background-removal-node';
+// import { removeBackground } from '@masuhajime/background-removal-node';
 // import imglyRemoveBackground from '@imgly/background-removal';
 import { Buffer } from 'buffer';
 // import { Rembg } from '@xixiyahaha/rembg-node';
@@ -10,7 +10,8 @@ const imageRemoveBackground = async (
   param: ImageRemoveBackgroundParameter,
 ): Promise<Buffer> => {
   const { buffer } = param;
-  // return buffer;
+  return buffer;
+
   const blob = new Blob([buffer], { type: 'image/png' });
 
   const { data, info } = await sharp(buffer)
@@ -56,26 +57,26 @@ const imageRemoveBackground = async (
       '1': `file://${path.join(path.resolve(), 'assets/imgly/')}`,
     });
 
-    const removed = await removeBackground(blob, {
-      publicPath: `file://${path.resolve(__dirname)}/../../node_modules/@masuhajime/background-removal-node/dist/`,
-      // publicPath: `file://${path.join(path.resolve(), 'assets/imgly/')}`,
-      // path.resolve() や__dirname でdev startとbulidのパスが違う, 実際に表示して試す方が良い
-      // publicPath: `file://${path.join(path.resolve(), 'node_modules/@masuhajime/background-removal-node/dist/')}`,
-      // publicPath: `file://${path.resolve(__dirname)}/../../../assets/imgly/`,
-      // .default(`file://${path.resolve(`node_modules/${pkg.name}/dist/`)}/`)
-      // publicPath: `file://${path.resolve(
-      //   __dirname,
-      // )}/../../node_modules/@masuhajime/background-removal-node/dist/`,
-      // )}/node_modules/@masuhajime/background-removal-node/dist/`,
-      // `file://${path.resolve(`resource/background-redmoval-node`)}/`,
-      // 'file://resource/background-removal-node/',
-      model: 'medium',
-      debug: true,
-      output: {
-        format: 'image/png',
-      },
-    });
-    output = sharp(await removed.arrayBuffer());
+    // const removed = await removeBackground(blob, {
+    //   publicPath: `file://${path.resolve(__dirname)}/../../node_modules/@masuhajime/background-removal-node/dist/`,
+    //   // publicPath: `file://${path.join(path.resolve(), 'assets/imgly/')}`,
+    //   // path.resolve() や__dirname でdev startとbulidのパスが違う, 実際に表示して試す方が良い
+    //   // publicPath: `file://${path.join(path.resolve(), 'node_modules/@masuhajime/background-removal-node/dist/')}`,
+    //   // publicPath: `file://${path.resolve(__dirname)}/../../../assets/imgly/`,
+    //   // .default(`file://${path.resolve(`node_modules/${pkg.name}/dist/`)}/`)
+    //   // publicPath: `file://${path.resolve(
+    //   //   __dirname,
+    //   // )}/../../node_modules/@masuhajime/background-removal-node/dist/`,
+    //   // )}/node_modules/@masuhajime/background-removal-node/dist/`,
+    //   // `file://${path.resolve(`resource/background-redmoval-node`)}/`,
+    //   // 'file://resource/background-removal-node/',
+    //   model: 'medium',
+    //   debug: true,
+    //   output: {
+    //     format: 'image/png',
+    //   },
+    // });
+    // output = sharp(await removed.arrayBuffer());
   } else {
     // const image = sharp(buffer);
     // const rembg = new Rembg({
